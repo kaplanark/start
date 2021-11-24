965c2919992b55ba4d790286dd6c444394ae9fce
kaplanark <kaplan.arkan@gmail.com>
2021-11-24 16:24:46 +0300

    24.11.2021

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-    <link rel="stylesheet" href="front/css/fontawesome.min.css">
-    <link rel="stylesheet" href="front/css/libs.css">
+    <title>Demo</title>
+    <link rel="stylesheet" href="front/css/fontawesome.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="stylesheet" href="front/css/libs.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <link rel="stylesheet" href="front/css/main.css">
 </head>
 <body>
