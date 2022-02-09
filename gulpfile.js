ea088f397f8d2943df03cc48592bb7eb14e2205d
kaplanark <kaplanarkan@gmail.com>
2022-02-09 21:57:42 +0300

    10.02.22

--- gulpfile.js
+++ gulpfile.js
@@ -10,106 +10,122 @@ const php = require('gulp-connect-php');
 const cleancss = require("gulp-clean-css");
 const rename = require('gulp-rename');
 const del = require("del");
+const fs = require('fs');
+const path = require('path');
 const reload = browserSync.reload;
 dotenv.config();
 
 function browser_sync() {
-  php.server({}, function (){
-    browserSync.init({
-      proxy: '127.0.0.1:8000'
+    php.server({}, function () {
+        browserSync.init({
+            proxy: '127.0.0.1:8000'
+        });
     });
-  });
 }
 
 function watch_files() {
-  gulp.watch('./*.html').on('change', reload)
-  gulp.watch('./*.php').on('change', reload)
-  // gulp.watch('./*.php').on('add', reload)
-  gulp.watch('./includes/**/*.php').on('change', reload)
-  gulp.watch('./front/js/*.js').on('change', reload)
-  gulp.watch('./front/scss/**/*.scss', gulp.series(libs_css,main_css))
+    gulp.watch('./*.html').on('change', reload)
+    gulp.watch('./*.php').on('change', reload)
+    // gulp.watch('./*.php').on('add', reload)
+    gulp.watch('./includes/**/*.php').on('change', reload)
+    gulp.watch('./front/js/*.js').on('change', reload)
+    gulp.watch('./front/scss/**/*.scss', gulp.series(libs_css, main_css))
 }
 
 function libs_css() {
-  return gulp.src('./front/scss/libs.scss')
-      .pipe(plumber([{errorHandler: false}]))
-      .pipe(sass())
-      .pipe(prefix('last 2 versions'))
-      .pipe(gulp.dest('./front/css/'))
-      .pipe(browserSync.stream())
+    return gulp.src('./front/scss/libs.scss')
+        .pipe(plumber([{errorHandler: false}]))
+        .pipe(sass())
+        .pipe(prefix('last 2 versions'))
+        .pipe(gulp.dest('./front/css/'))
+        .pipe(browserSync.stream())
 }
 
 function main_css() {
-  return gulp.src('./front/scss/main.scss')
-      .pipe(plumber([{errorHandler: false}]))
-      .pipe(sass())
-      .pipe(prefix('last 2 versions'))
-      .pipe(gulp.dest('./front/css/'))
-      .pipe(browserSync.stream())
+    return gulp.src('./front/scss/main.scss')
+        .pipe(plumber([{errorHandler: false}]))
+        .pipe(sass())
+        .pipe(prefix('last 2 versions'))
+        .pipe(gulp.dest('./front/css/'))
+        .pipe(browserSync.stream())
 }
 
 
 //use 'gulp deploy' command to deploy
-gulp.task( 'deploy', function () {
-  var conn = ftp.create({
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    port:21,
-    parallel: 1,
-    log: gutil.log
-  });
+gulp.task('deploy', function () {
+    let dirname = path.join(__dirname).replace(/^.*[\\\/]/, '');
+    let conn = ftp.create({
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        port: 21,
+        parallel: 1,
+        log: gutil.log
+    });
 
-  var globs = [
-    'front/**',
-    'includes/**',
-    'index.php'
-  ];
+    let globs = [
+        'front/**',
+        'includes/**',
+        'index.php'
+    ];
 
-  return gulp.src(globs, { base: '.', buffer: false,dot:true})
-    .pipe(conn.newer('/public_html/project_name')) // only upload newer files
-    .pipe(conn.dest('/public_html/project_name'));
+    return gulp.src(globs, {base: '.', buffer: false, dot: true})
+        .pipe(conn.newer('/public_html/' + `${dirname}`)) // only upload newer files
+        .pipe(conn.dest('/public_html/' + `${dirname}`));
 
 });
 
 //for minfiyed css 
-gulp.task("minify",() => {
-  del(["./front/css/main.css","./front/css/main.min.css"]);
-  return gulp
-    .src("./front/css/main.css")
-    .pipe(
-      cleancss({ debug: true }, (details) => {
-        console.log(`${details.name}: ${details.stats.originalSize}`);
-        console.log('/main.min.css: ' + `${details.stats.minifiedSize}`);
-      })
-    )
-    .pipe(
-      rename({
-        suffix: ".min",
-      })
-    )
-    .pipe(gulp.dest("./front/css/"));
+gulp.task("minify", () => {
+    del(["./front/css/main.css", "./front/css/main.min.css"]);
+    return gulp
+        .src("./front/css/main.css")
+        .pipe(
+            cleancss({debug: true}, (details) => {
+                console.log(`${details.name}: ${details.stats.originalSize}`);
+                console.log('/main.min.css: ' + `${details.stats.minifiedSize}`);
+            })
+        )
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(gulp.dest("./front/css/"));
 });
 
-gulp.task("minify-libs",() => {
-  del(["./front/css/libs.css","./front/css/libs.min.css"]);
-  return gulp
-    .src("./front/css/libs.css")
-    .pipe(
-      cleancss({ debug: true }, (details) => {
-        console.log(`${details.name}: ${details.stats.originalSize}`);
-        console.log('/libs.min.css: ' + `${details.stats.minifiedSize}`);
-      })
-    )
-    .pipe(
-      rename({
-        suffix: ".min",
-      })
-    )
-    .pipe(gulp.dest("./front/css/"));
+gulp.task("minify-libs", () => {
+    del(["./front/css/libs.css", "./front/css/libs.min.css"]);
+    return gulp
+        .src("./front/css/libs.css")
+        .pipe(
+            cleancss({debug: true}, (details) => {
+                console.log(`${details.name}: ${details.stats.originalSize}`);
+                console.log('/libs.min.css: ' + `${details.stats.minifiedSize}`);
+            })
+        )
+        .pipe(
+            rename({
+                suffix: ".min",
+            })
+        )
+        .pipe(gulp.dest("./front/css/"));
+});
+
+gulp.task('version', function () {
+    return new Promise(function (resolve, reject) {
+        fs.readFile('.version.txt', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(data.toString());
+            }
+        });
+        resolve();
+    });
 });
 
 const browser = gulp.parallel(browser_sync, watch_files);
 
-exports.default = gulp.parallel(browser, libs_css,main_css);
+exports.default = gulp.parallel(browser, libs_css, main_css);
 
